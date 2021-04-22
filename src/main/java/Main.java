d7d275c05ce4a1a5dd6b4744a2c0e128e8fcf405
itoshuhei <ito.shuhei@tis.co.jp>
2021-04-23 00:18:41 +0900

    メインメソッドを新規作成

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import Service.MemberServiceImpl;
+import entity.Member;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+        MemberServiceImpl service = new MemberServiceImpl();
+        System.out.println(service.greet(2));
+        System.out.println(service.getAll());
+        ArrayList<Member> list = service.getAll();
+        for(Member mem : list){
+            System.out.println(mem.getId() + ","  + mem.getName() + "," + mem.getEmail());
+        }
+    }
+}
--- src/main/java/Service/MemberServiceImpl.java
+++ src/main/java/Service/MemberServiceImpl.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 public class MemberServiceImpl implements MemberService{
 
     @Override
-    public String greet(int i) {
+    public String greet(int i){
         String[] greetings = {"Good Morning", "Hello", "Good Evening"};
         return greetings[i];
     }
 
     @Override
-    public ArrayList<Member> getAll() {
+    public ArrayList<Member> getAll(){
         ArrayList<Member> list = new ArrayList<>();
         Member mem1 = new Member(1, "Linda", "linda@example.com");
         Member mem2 = new Member(2, "James", "james@example.com");
